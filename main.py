7c7816df03501668a70d48158a758ee4c6d2466a
Marcelo Elizeche Landó <melizeche@gmail.com>
2024-01-12 21:21:51 -0300

    Agregar comentario y linea ----

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def saludo(nombre="Mundo!"):
+def saludo(nombre="Mundo!"):  #Funcion para saludar
     print(f"Hola {nombre}")
 
 saludo("Marce")
 saludo()
+print("------------")
 saludo("Python")
 saludo("git y github")
